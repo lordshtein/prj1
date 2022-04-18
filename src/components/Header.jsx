bcea913eb231c0c9dc8ed7faf6bd59b25514ad17
lordshtein <lordshtein@gmail.com>
2022-04-18 11:48:06 +0300

    copy files

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return <header className="header">
+        < img src="https://png.pngtree.com/element_origin_min_pic/17/02/09/0148d538a98f97fe34af23524e26b536.jpg" />
+    </header>
+}
+
+export default Header;
